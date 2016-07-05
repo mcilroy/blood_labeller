b29a4d40a0bb4354ae703e92ad404fb6bd5ac1f6
mcilroy <mcilroy23@gmail.com>
2016-07-05 18:07:03 -0300

    minor change to creation of dataset for convnet

--- inspect_labelled_data.py
+++ inspect_labelled_data.py
@@ -19,6 +19,8 @@ def main():
     eosinophils = create_np_array(entries, the_db, constants.EOSINOPHIL)
     lymphocytes = create_np_array(entries, the_db, constants.LYMPHOCYTE)
     filename, ext = os.path.splitext(FILE_NAME)
+    print("neutro", len(neutrophils), "mono", len(monocytes), "baso", len(basophils), "eosin", len(eosinophils),
+          "lympho", len(lymphocytes))
     np.savez(os.path.join(DATA_LOCATION, filename+"_cleaned3"), neutrophils=neutrophils, monocytes=monocytes, basophils=basophils,
              eosinophils=eosinophils, lymphocytes=lymphocytes)
 
@@ -27,8 +29,9 @@ def create_np_array(entries, the_db, name):
     cells = np.zeros([len(entries), the_db.training_images[0].shape[1], the_db.training_images[0].shape[2],
                             the_db.training_images[0].shape[3]])
     count_cells = 0
+    exclude_indexes = [15]  # exclude patient 15 because it looks weird!
     for i, val in enumerate(entries):
-        if val.cell_type == name:
+        if val.cell_type == name and val.patient_index not in exclude_indexes:
             cells[count_cells, :, :, :] = the_db.training_images[val.patient_index][val.index_in_array, :, :, :]
             count_cells += 1
     cells = cells[0:count_cells, :, :, :]
